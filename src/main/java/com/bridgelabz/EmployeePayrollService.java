8a01809b65ee6c6542c207664ff1aa19928615b6
AnjaliRawke <anjalirawke21@gmail.com>
2023-04-11 19:52:59 +0530

    Added:Employee Payroll Service Problem

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+	private List<EmployeePayroll> employeePayrollList;
+
+	public EmployeePayrollService(){}
+
+	public EmployeePayrollService(List<EmployeePayroll> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<EmployeePayroll> employeePayrollList = new ArrayList<>();
+
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+
+		Scanner consoleInputReader = new Scanner(System.in);
+
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.print("Enter Employee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.print("Enter Employee Name: ");
+		String name = consoleInputReader.next();
+		System.out.print("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+
+		employeePayrollList.add(new EmployeePayroll(id, name, salary));
+	}
+
+	private void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+	}
+}
